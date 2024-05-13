c795cac009375cf3b56a4e206389aee3bd92c677
lllllllillllllillll <lllllllillllllillll@proton.me>
2024-05-13 00:18:39 -0700

    created compose modal. updated import instructions

--- controllers/apps.js
+++ controllers/apps.js
@@ -48,6 +48,7 @@ export const Apps = async (req, res) => {
       appCard = appCard.replace(/AppDesc/g, 'Compose File');
       appCard = appCard.replace(/AppLogo/g, `https://raw.githubusercontent.com/lllllllillllllillll/DweebUI-Icons/main/${service_name}.png`);
       appCard = appCard.replace(/AppCategories/g, '<span class="badge bg-orange-lt">Compose</span> ');
+      appCard = appCard.replace(/AppType/g, 'compose');
       apps_list += appCard;
     });
   } else {
@@ -82,6 +83,7 @@ export const Apps = async (req, res) => {
           appCard = appCard.replace(/AppDesc/g, desc);
           appCard = appCard.replace(/AppLogo/g, logo);
           appCard = appCard.replace(/AppCategories/g, categories);
+          appCard = appCard.replace(/AppType/g, 'json');
           apps_list += appCard;
       }
     }
@@ -244,187 +246,199 @@ function CatagoryColor(category) {
 
 export const InstallModal = async (req, res) => {
   let input = req.header('hx-trigger-name');
-  let result = templates_global.find(t => t.name == input);
+  let type = req.header('hx-trigger');
 
-  let name = result.name || result.title.toLowerCase();
-  let short_name = name.slice(0, 25) + "...";
-  let desc = result.description.replaceAll(". ", ".\n") || "no description available";
-  let short_desc = desc.slice(0, 60) + "...";
-  let modal_name = name.replaceAll(" ", "-");
-  let form_id = name.replaceAll("-", "_");
-  let note = result.note ? result.note.replaceAll(". ", ".\n") : "no notes available";
-  let command = result.command ? result.command : "";
-  let command_check = command ? "checked" : "";
-  let privileged = result.privileged || "";
-  let privileged_check = privileged ? "checked" : "";
-  let repository = result.repository || "";
-  let image = result.image || "";
-  let net_host, net_bridge, net_docker = '';
-  let net_name = 'AppBridge';
-  let restart_policy = result.restart_policy || 'unless-stopped';
-  
-  switch (result.network) {
-    case 'host':
-      net_host = 'checked';
-      break;
-    case 'bridge':
-      net_bridge = 'checked';
-      net_name = result.network;
-      break;
-    default:
-      net_docker = 'checked';
-  }
-
-  if (repository != "") {
-    image = (`${repository.url}/raw/master/${repository.stackfile}`);
-  }
-
-  let [ports_data, volumes_data, env_data, label_data] = [[], [], [], []];
-
-  for (let i = 0; i < 12; i++) {
-    
-    // Get port details
-    try {
-      let ports = result.ports[i];
-      let port_check = ports ? "checked" : "";
-      let port_external = ports.split(":")[0] ? ports.split(":")[0] : ports.split("/")[0];
-      let port_internal = ports.split(":")[1] ? ports.split(":")[1].split("/")[0] : ports.split("/")[0];
-      let port_protocol = ports.split("/")[1] ? ports.split("/")[1] : "";
-
-      // remove /tcp or /udp from port_external if it exists
-      if (port_external.includes("/")) {
-        port_external = port_external.split("/")[0];
-      }
+  if (type == 'compose') {
+    let compose = readFileSync(`templates/compose/${input}/compose.yaml`, 'utf8');
+    let modal = readFileSync('./views/modals/compose.html', 'utf8');
+    modal = modal.replace(/COMPOSE_CONTENT/g, compose);
+    res.send(modal);
+    return;
+  } else {
       
-      ports_data.push({
-        check: port_check,
-        external: port_external,
-        internal: port_internal,
-        protocol: port_protocol
-      });
-    } catch {
-        ports_data.push({
-          check: "",
-          external: "",
-          internal: "",
-          protocol: ""
-        });
+
+    let result = templates_global.find(t => t.name == input);
+
+    let name = result.name || result.title.toLowerCase();
+    let short_name = name.slice(0, 25) + "...";
+    let desc = result.description.replaceAll(". ", ".\n") || "no description available";
+    let short_desc = desc.slice(0, 60) + "...";
+    let modal_name = name.replaceAll(" ", "-");
+    let form_id = name.replaceAll("-", "_");
+    let note = result.note ? result.note.replaceAll(". ", ".\n") : "no notes available";
+    let command = result.command ? result.command : "";
+    let command_check = command ? "checked" : "";
+    let privileged = result.privileged || "";
+    let privileged_check = privileged ? "checked" : "";
+    let repository = result.repository || "";
+    let image = result.image || "";
+    let net_host, net_bridge, net_docker = '';
+    let net_name = 'AppBridge';
+    let restart_policy = result.restart_policy || 'unless-stopped';
+    
+    switch (result.network) {
+      case 'host':
+        net_host = 'checked';
+        break;
+      case 'bridge':
+        net_bridge = 'checked';
+        net_name = result.network;
+        break;
+      default:
+        net_docker = 'checked';
     }
 
-    // Get volume details
-    try {
-      let volumes = result.volumes[i];
-      let volume_check = volumes ? "checked" : "";
-      let volume_bind = volumes.bind ? volumes.bind : "";
-      let volume_container = volumes.container ? volumes.container.split(":")[0] : "";
-      let volume_readwrite = "rw";
+    if (repository != "") {
+      image = (`${repository.url}/raw/master/${repository.stackfile}`);
+    }
 
-      if (volumes.readonly == true) {
-        volume_readwrite = "ro";
+    let [ports_data, volumes_data, env_data, label_data] = [[], [], [], []];
+
+    for (let i = 0; i < 12; i++) {
+      
+      // Get port details
+      try {
+        let ports = result.ports[i];
+        let port_check = ports ? "checked" : "";
+        let port_external = ports.split(":")[0] ? ports.split(":")[0] : ports.split("/")[0];
+        let port_internal = ports.split(":")[1] ? ports.split(":")[1].split("/")[0] : ports.split("/")[0];
+        let port_protocol = ports.split("/")[1] ? ports.split("/")[1] : "";
+
+        // remove /tcp or /udp from port_external if it exists
+        if (port_external.includes("/")) {
+          port_external = port_external.split("/")[0];
+        }
+        
+        ports_data.push({
+          check: port_check,
+          external: port_external,
+          internal: port_internal,
+          protocol: port_protocol
+        });
+      } catch {
+          ports_data.push({
+            check: "",
+            external: "",
+            internal: "",
+            protocol: ""
+          });
       }
 
-      volumes_data.push({
-        check: volume_check,
-        bind: volume_bind,
-        container: volume_container,
-        readwrite: volume_readwrite
-      });
-    } catch {
-      volumes_data.push({
-        check: "",
-        bind: "",
-        container: "",
-        readwrite: ""
-      });
+      // Get volume details
+      try {
+        let volumes = result.volumes[i];
+        let volume_check = volumes ? "checked" : "";
+        let volume_bind = volumes.bind ? volumes.bind : "";
+        let volume_container = volumes.container ? volumes.container.split(":")[0] : "";
+        let volume_readwrite = "rw";
+
+        if (volumes.readonly == true) {
+          volume_readwrite = "ro";
+        }
+
+        volumes_data.push({
+          check: volume_check,
+          bind: volume_bind,
+          container: volume_container,
+          readwrite: volume_readwrite
+        });
+      } catch {
+        volumes_data.push({
+          check: "",
+          bind: "",
+          container: "",
+          readwrite: ""
+        });
+      }
+
+      // Get environment details
+      try {
+        let env = result.env[i];
+        let env_check = "";
+        let env_default = env.default ? env.default : "";
+        if (env.set) { env_default = env.set;}
+        let env_description = env.description ? env.description : "";
+        let env_label = env.label ? env.label : "";
+        let env_name = env.name ? env.name : "";
+
+        env_data.push({
+          check: env_check,
+          default: env_default,
+          description: env_description,
+          label: env_label,
+          name: env_name
+        });
+      } catch {
+        env_data.push({
+          check: "",
+          default: "",
+          description: "",
+          label: "",
+          name: ""
+        });
+      }
+
+      // Get label details
+      try {
+        let label = result.labels[i];
+        let label_check = "";
+        let label_name = label.name ? label.name : "";
+        let label_value = label.value ? label.value : "";
+
+        label_data.push({
+          check: label_check,
+          name: label_name,
+          value: label_value
+        });
+      } catch {
+        label_data.push({
+          check: "",
+          name: "",
+          value: ""
+        });
+      }
+
     }
-
-    // Get environment details
-    try {
-      let env = result.env[i];
-      let env_check = "";
-      let env_default = env.default ? env.default : "";
-      if (env.set) { env_default = env.set;}
-      let env_description = env.description ? env.description : "";
-      let env_label = env.label ? env.label : "";
-      let env_name = env.name ? env.name : "";
-
-      env_data.push({
-        check: env_check,
-        default: env_default,
-        description: env_description,
-        label: env_label,
-        name: env_name
-      });
-    } catch {
-      env_data.push({
-        check: "",
-        default: "",
-        description: "",
-        label: "",
-        name: ""
-      });
-    }
-
-    // Get label details
-    try {
-      let label = result.labels[i];
-      let label_check = "";
-      let label_name = label.name ? label.name : "";
-      let label_value = label.value ? label.value : "";
-
-      label_data.push({
-        check: label_check,
-        name: label_name,
-        value: label_value
-      });
-    } catch {
-      label_data.push({
-        check: "",
-        name: "",
-        value: ""
-      });
-    }
-
-  }
-  
-  let modal = readFileSync('./views/modals/install.html', 'utf8');
-  modal = modal.replace(/AppName/g, name);
-  modal = modal.replace(/AppNote/g, note);
-  modal = modal.replace(/AppImage/g, image);
-  modal = modal.replace(/RestartPolicy/g, restart_policy);
-  modal = modal.replace(/NetHost/g, net_host);
-  modal = modal.replace(/NetBridge/g, net_bridge);
-  modal = modal.replace(/NetDocker/g, net_docker);
-  modal = modal.replace(/NetName/g, net_name);
-  modal = modal.replace(/ModalName/g, modal_name);
-  modal = modal.replace(/FormId/g, form_id);
-  modal = modal.replace(/CommandCheck/g, command_check);
-  modal = modal.replace(/CommandValue/g, command);
-  modal = modal.replace(/PrivilegedCheck/g, privileged_check);
-
-
-  for (let i = 0; i < 12; i++) {
-    modal = modal.replaceAll(`Port${i}Check`, ports_data[i].check);
-    modal = modal.replaceAll(`Port${i}External`, ports_data[i].external);
-    modal = modal.replaceAll(`Port${i}Internal`, ports_data[i].internal);
-    modal = modal.replaceAll(`Port${i}Protocol`, ports_data[i].protocol);
-
-    modal = modal.replaceAll(`Volume${i}Check`, volumes_data[i].check);
-    modal = modal.replaceAll(`Volume${i}Bind`, volumes_data[i].bind);
-    modal = modal.replaceAll(`Volume${i}Container`, volumes_data[i].container);
-    modal = modal.replaceAll(`Volume${i}RW`, volumes_data[i].readwrite);
     
-    modal = modal.replaceAll(`Env${i}Check`, env_data[i].check);
-    modal = modal.replaceAll(`Env${i}Default`, env_data[i].default);
-    modal = modal.replaceAll(`Env${i}Description`, env_data[i].description);
-    modal = modal.replaceAll(`Env${i}Label`, env_data[i].label);
-    modal = modal.replaceAll(`Env${i}Name`, env_data[i].name);
+    let modal = readFileSync('./views/modals/json.html', 'utf8');
+    modal = modal.replace(/AppName/g, name);
+    modal = modal.replace(/AppNote/g, note);
+    modal = modal.replace(/AppImage/g, image);
+    modal = modal.replace(/RestartPolicy/g, restart_policy);
+    modal = modal.replace(/NetHost/g, net_host);
+    modal = modal.replace(/NetBridge/g, net_bridge);
+    modal = modal.replace(/NetDocker/g, net_docker);
+    modal = modal.replace(/NetName/g, net_name);
+    modal = modal.replace(/ModalName/g, modal_name);
+    modal = modal.replace(/FormId/g, form_id);
+    modal = modal.replace(/CommandCheck/g, command_check);
+    modal = modal.replace(/CommandValue/g, command);
+    modal = modal.replace(/PrivilegedCheck/g, privileged_check);
 
-    modal = modal.replaceAll(`Label${i}Check`, label_data[i].check);
-    modal = modal.replaceAll(`Label${i}Name`, label_data[i].name);
-    modal = modal.replaceAll(`Label${i}Value`, label_data[i].value);
-  }
+
+    for (let i = 0; i < 12; i++) {
+      modal = modal.replaceAll(`Port${i}Check`, ports_data[i].check);
+      modal = modal.replaceAll(`Port${i}External`, ports_data[i].external);
+      modal = modal.replaceAll(`Port${i}Internal`, ports_data[i].internal);
+      modal = modal.replaceAll(`Port${i}Protocol`, ports_data[i].protocol);
+
+      modal = modal.replaceAll(`Volume${i}Check`, volumes_data[i].check);
+      modal = modal.replaceAll(`Volume${i}Bind`, volumes_data[i].bind);
+      modal = modal.replaceAll(`Volume${i}Container`, volumes_data[i].container);
+      modal = modal.replaceAll(`Volume${i}RW`, volumes_data[i].readwrite);
+      
+      modal = modal.replaceAll(`Env${i}Check`, env_data[i].check);
+      modal = modal.replaceAll(`Env${i}Default`, env_data[i].default);
+      modal = modal.replaceAll(`Env${i}Description`, env_data[i].description);
+      modal = modal.replaceAll(`Env${i}Label`, env_data[i].label);
+      modal = modal.replaceAll(`Env${i}Name`, env_data[i].name);
+
+      modal = modal.replaceAll(`Label${i}Check`, label_data[i].check);
+      modal = modal.replaceAll(`Label${i}Name`, label_data[i].name);
+      modal = modal.replaceAll(`Label${i}Value`, label_data[i].value);
+    }
   res.send(modal);
+  }
 }
 
 
